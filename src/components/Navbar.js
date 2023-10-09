ba93967c62c42516a15605bfc53fbd0b539a4211
orangeguppy <88083712+orangeguppy@users.noreply.github.com>
2023-10-09 23:28:30 +0800

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,21 +9,17 @@ function Navbar() {
         </div>
         <div class="flex-none">
           <ul class="menu menu-horizontal px-1" style={{ zIndex: 10 }}>
-            <li><Link to="/">Tool</Link></li>
             <li tabindex="0">
               <a>
                 Documentation
                 <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg>
               </a>
               <ul class="p-2 bg-base-100">
-                <li><Link to="/plan">Project Plan</Link></li>
+                <li><Link to="/">Project Plan</Link></li>
                 <li><Link to="/eda">Exploratory Data Analysis</Link></li>
                 <li><Link to="/data-prep">Data Preparation</Link></li>
-                <li><a>Training and Testing</a></li>
-                <li><a>Model Evauation</a></li>
               </ul>
             </li>
-            <li><a>Feedback</a></li>
           </ul>
         </div>
       </div>
